f137eb5bf64cdcf6adc20a54a3125c116fb9b261
hrkanon <habibur3753@gmail.com>
2021-09-02 17:37:46 +0600

    code added

--- js/app.js
+++ js/app.js
@@ -8,46 +8,48 @@ spinner.style.display = "none";
 const searchField = document.getElementById("search-field");
 
 const searchItem = () => {
-  // const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
-  //   clear searchField
+  //   clear searchField and previous results
   searchField.value = "";
   resultQuantity.textContent = "";
   searchedDisplay.textContent = "";
-  // errorMessage.textContent = "";
+  // Display Spineer
   spinner.style.display = "block";
-  console.log(searchText);
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
   console.log(url);
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayBooks(data));
 };
+
 const displayBooks = (books) => {
   // console.log(books);
   spinner.style.display = "none";
   searchedDisplay.textContent = "";
+  // Handle empty search request
   if (books.q === "") {
     errorMessage.style.display = "block";
-  } else {
+  }
+  // Handle if no result available
+  else if (books.docs.length === 0) {
     errorMessage.style.display = "none";
-    console.log(books.docs);
-    if (books.docs.length === 0) {
-      resultQuantity.innerText = `No result found`;
-    } else {
-      resultQuantity.innerText = `Items found - ${books.docs.length} of ${books.numFound} for ${searchField.value} `;
-    }
-
+    resultQuantity.innerText = `No result found`;
+  }
+  // Handle if results available
+  else {
+    // hide error message
+    errorMessage.style.display = "none";
+    // displaying total quantity of search
+    resultQuantity.innerText = `Items found - ${books.docs.length}`;
+    // getting 24 items of total quantity
     const sortedBooks = books.docs.slice(0, 24);
     sortedBooks.forEach((book) => {
-      // resultQuantity.innerText = ``;
-
-      // console.log(book);
+      // console.log(book.publisher[0]);
       const div = document.createElement("div");
       div.classList.add("col");
       div.innerHTML = `
          <div class="card h-100">
-               <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+               <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt=>
             <div class="card-body">
                <h5 class="card-title">Book Name: ${book.title}</h5>
                <p class="card-text">Author: ${book.author_name}</p>
